cf25f6e660f0e482eb62579afb5e6e711bf1a08d
ryun <ryan@letsgolearn.com>
2022-03-20 21:35:44 -0700

    make field collection json serializable

--- src/Fields/FieldCollection.php
+++ src/Fields/FieldCollection.php
@@ -2,21 +2,31 @@
 
 namespace Humweb\Table\Fields;
 
-use Illuminate\Contracts\Support\Arrayable;
+
 use Illuminate\Support\Collection;
 use JsonSerializable;
 
-class FieldCollection extends Collection
+class FieldCollection extends Collection implements JsonSerializable
 {
 
     public function find($attribute)
     {
         return $this->firstWhere('attribute', $attribute);
     }
+
     public function toArray()
     {
         return $this->map(function ($value) {
             return $value instanceof JsonSerializable ? $value->jsonSerialize() : $value;
         })->all();
+    }
+
+
+    /**
+     * @return array|mixed[]
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
     }
 }
