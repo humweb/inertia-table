d2daf39b765138b72b80ef7cd54411d8a2ffa83b
ryun <ryan@letsgolearn.com>
2023-01-17 01:49:55 -0800

    add some more tests for filters
    Signed-off-by: ryun <ryan@letsgolearn.com>

---
+++ tests/Unit/SelectFilterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Humweb\Table\Tests\Unit;
+
+use Humweb\Table\Filters\SelectFilter;
+use Humweb\Table\Tests\TestCase;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
+
+class SelectFilterTest extends TestCase
+{
+
+
+    public function test_it_can_build_validation_rules()
+    {
+        $filter = SelectFilter::make('email', 'Email', [1,2,3]);
+        $this->assertFalse($filter->whereHas);
+        $filter->whereHas();
+        $this->assertTrue($filter->whereHas);
+    }
+
+    public function test_it_can_apply_whereHas_filter()
+    {
+        $filter = SelectFilter::make('email', 'Email', [1,2,3])->whereHas();
+
+        $request = $this->request(function (Request $request) {
+            $request->query->set('filters', ['email' => 'user@email.com']);
+        });
+
+        $mockedBuilder = mock(TestBuilder::class)
+            ->shouldReceive('where')
+            ->with(['email', 'foo'])
+            ->shouldReceive('whereHas')
+            ->once()
+            ->shouldReceive('getConnection')
+            ->andReturn(new TestConnection(''))
+            ->getMock();
+        $filter->apply($request, $mockedBuilder, 'foo');
+    }
+}
+
---
+++ tests/Unit/TrashedFilterTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Humweb\Table\Tests\Unit;
+
+use Humweb\Table\Filters\SelectFilter;
+use Humweb\Table\Filters\TrashedFilter;
+use Humweb\Table\Tests\TestCase;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
+
+class TrashedFilterTest extends TestCase
+{
+
+
+
+    public function test_it_can_apply_trashed_filter()
+    {
+        $filter = TrashedFilter::make('email', 'Email');
+
+        $request = $this->request();
+
+        $mockedBuilder = mock(TestBuilder::class)
+            ->shouldReceive('withTrashed')
+            ->once()
+            ->shouldReceive('getConnection')
+            ->andReturn(new TestConnection(''))
+            ->getMock();
+
+        $filter->apply($request, $mockedBuilder, 'with');
+
+        $mockedBuilder = mock(TestBuilder::class)
+            ->shouldReceive('onlyTrashed')
+            ->once()
+            ->shouldReceive('getConnection')
+            ->andReturn(new TestConnection(''))
+            ->getMock();
+
+        $filter->apply($request, $mockedBuilder, 'only');
+    }
+}
