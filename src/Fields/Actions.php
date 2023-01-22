7e00638e8881425d772285650977729c5e4d5312
ryun <ryan@letsgolearn.com>
2023-01-21 19:36:41 -0800

    add actions
    Signed-off-by: ryun <ryan@letsgolearn.com>

---
+++ src/Fields/Actions.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Humweb\Table\Fields;
+
+class Actions extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public string $component = 'action-field';
+
+    /**
+     * Create a new field.
+     *
+     * @param  string|null  $name
+     * @param  string|null  $attribute
+     * @return void
+     */
+    public function __construct($name = null, $attribute = null)
+    {
+        parent::__construct($name ?? 'Actions', $attribute);
+    }
+
+    public function actions(array $actions)
+    {
+        $this->meta['actions'] = $actions;
+        return $this;
+    }
+}
