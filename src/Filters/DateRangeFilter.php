3c492a99db03faa234b2791cc2f702a677f98a35
ryun <ryan@letsgolearn.com>
2023-02-03 01:37:53 -0800

    add date range filter
    Signed-off-by: ryun <ryan@letsgolearn.com>

---
+++ src/Filters/DateRangeFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Humweb\Table\Filters;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class DateRangeFilter extends Filter
+{
+    /**
+     * @var string
+     */
+    public string $component = 'date-range-filter';
+
+    public string $dateFormat = '';
+
+    /**
+     * @param  Request       $request
+     * @param  Builder       $query
+     * @param  string|array  $value
+     *
+     * @return $this|Filter
+     */
+    public function apply(Request $request, Builder $query, $value)
+    {
+
+        if (is_string($value)) {
+            $value = explode('-', str_replace(' ', '', $value));
+        }
+
+        $this->value = [
+            Carbon::createFromFormat($this->dateFormat, $value[0])->startOfDay(),
+            Carbon::createFromFormat($this->dateFormat, $value[1])->endOfDay()
+        ];
+
+        $query->whereBetween($this->field, $this->value);
+
+        return $this;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return array_merge([
+            'component'  => $this->component,
+            'field'      => $this->field,
+            'label'      => $this->label,
+            'value'      => $this->value,
+            'rules'      => $this->rules,
+            'dateFormat' => $this->dateFormat
+        ], $this->meta());
+    }
+}
